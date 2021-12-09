af55fdc867ff6747089dd748250372639bbee6f4
chavez <95760172+osum-posum@users.noreply.github.com>
2021-12-09 01:51:20 -0500

    Add files via upload

---
+++ User.js
@@ -0,0 +1,18 @@
+import '../App.css';
+import { useRef, useState } from 'react';   
+import axios from 'axios';
+import { useNavigate } from 'react-router';
+
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+const User = () => {
+
+    return (
+        <Container>
+
+        </Container>
+    );
+};
+
+export default User;
