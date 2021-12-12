eb3366739d5040e9aa334246ca4775f4ddf44aa8
chavez <95760172+osum-posum@users.noreply.github.com>
2021-12-12 11:12:18 -0500

    1.8

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './elements/Home.js';
-
+import User from './elements/User.js'
 
 function App() {
 
@@ -8,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/User/:id' element={<User />} />
       </Routes>
     </Router>
   );
--- User.js
+++ User.js
@@ -1,5 +1,5 @@
-import '../App.css';
-import { useRef, useState } from 'react';   
+import '../css/App.css';
+import { useRef, useState, useEffect } from 'react';   
 import axios from 'axios';
 import { useNavigate } from 'react-router';
 
@@ -8,8 +8,22 @@ import Button from 'react-bootstrap/Button';
 
 const User = () => {
 
+    // const [loginStatus, setLoginStatus] = useState('');
+
+    axios.defaults.withCredentials = true;
+
+    // Function --> sets loginStatus for useState(); 
+    // useEffect(() => {
+    //     axios.get('http://localhost:3001/login').then((response) => {
+    //         if (response.data.loggedIn === true) {
+    //             setLoginStatus(response.data.user[0].username);
+    //             console.log(response.data);
+    //         }
+    //     });
+    // }, []);
+
     return (
-        <Container>
+        <Container id='container' className='d-grid h-100'>
             <p>hi</p>
         </Container>
     );
