d8c03d564446ec1fbb0572384d6157a513246536
chavez <95760172+osum-posum@users.noreply.github.com>
2021-12-13 00:02:00 -0500

    1.9

--- User.js
+++ User.js
@@ -3,29 +3,53 @@ import { useRef, useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
 
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-
 const User = () => {
 
     // const [loginStatus, setLoginStatus] = useState('');
 
     axios.defaults.withCredentials = true;
 
-    // Function --> sets loginStatus for useState(); 
-    // useEffect(() => {
-    //     axios.get('http://localhost:3001/login').then((response) => {
-    //         if (response.data.loggedIn === true) {
-    //             setLoginStatus(response.data.user[0].username);
-    //             console.log(response.data);
-    //         }
-    //     });
-    // }, []);
-
+    
     return (
-        <Container id='container' className='d-grid h-100'>
-            <p>hi</p>
-        </Container>
+        <div className='container'>
+            <div className='row mt-4'>
+                <div className='col offset-1'>
+                    <form className='form-inline'>
+                        <div className='form-group'>
+                            <label>item</label>
+                            <input className='input--css2'></input>
+
+                            <label >spent</label>
+                            <input className='input--css2'></input>
+
+                            <label >net</label>
+                            <input className='input--css2'></input>
+
+                            <label>date</label>
+                            <input className='input--css2'></input>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div className='row mt-4'>
+                <div className='table'>
+                    <thead>
+                        <tr className='table--style'>
+                            <th className='table--bro' scope='col'>item</th>
+                            <th className='table--bro' scope='col'>spent</th>
+                            <th className='table--bro' scope='col'>net</th>
+                            <th className='table--bro' scope='col'>date</th>
+                        </tr>
+                        <tbody className='tbody'>
+                            <tr>
+                                <th></th>
+                            </tr>
+                        </tbody>
+                    </thead>
+                </div>
+            </div>
+        </div>
     );
 };
 
