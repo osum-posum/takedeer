dce293972b7197d5f3e5be803930db02cbb02981
chavez <95760172+osum-posum@users.noreply.github.com>
2021-12-15 16:46:29 -0500

    2.0

--- User.js
+++ User.js
@@ -1,39 +1,81 @@
 import '../css/App.css';
-import { useRef, useState, useEffect } from 'react';   
+import { useState, useEffect } from 'react';   
 import axios from 'axios';
 import { useNavigate } from 'react-router';
 
-const User = () => {
+const User = ({ item, spent, net, date }) => {
 
-    // const [loginStatus, setLoginStatus] = useState('');
+    const [name, setName] = useState('');
 
+    const [list, setList]= useState({
+        item: '',
+        spent: 0,
+        net: 0,
+        date: new Date()
+    });
+
+    const addItem = () => {
+        console.log(list)
+        console.log('im working')
+    }
+    
+    const handleChange = (e) => {
+        setList({
+            ...list,
+            [e.target.name]: e.target.value
+        });
+    };
+    
     axios.defaults.withCredentials = true;
 
-    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    // function sets users name
+    useEffect(() => {
+        axios.get('http://localhost:3001/login').then((response) => {
+            if (response.data.loggedIn === true) {
+                setName(response.data.user[0].username);
+                console.log(response.data);
+            }
+        });
+    }, []);
+
     return (
         <div className='container'>
+
+            <div className='row mt-4'>
+                <div className='col'>
+                    <label className='pink'>hi {name}</label>
+                    <button className='btn-sharp offset-10'>log out</button>
+                </div>
+            </div>
+
             <div className='row mt-4'>
                 <div className='col offset-1'>
-                    <form className='form-inline'>
+                    <form onSubmit={handleSubmit} className='form-inline'>
                         <div className='form-group'>
                             <label>item</label>
-                            <input className='input--css2'></input>
+                            <input onChange={handleChange} name='item' type='name' className='input--css2'></input>
 
                             <label >spent</label>
-                            <input className='input--css2'></input>
+                            <input onChange={handleChange} name='spent' type='number' step='any' className='input--css2'></input>
 
                             <label >net</label>
-                            <input className='input--css2'></input>
+                            <input onChange={handleChange} name='net' type='number' step='any' className='input--css2'></input>
 
                             <label>date</label>
-                            <input className='input--css2'></input>
+                            <input onChange={handleChange} name='date' type='date' className='input--css2'></input>
+
+                            <button onClick={addItem} className='btn-sharp'>enter</button>
                         </div>
                     </form>
                 </div>
             </div>
 
             <div className='row mt-4'>
-                <div className='table'>
+                <table>
                     <thead>
                         <tr className='table--style'>
                             <th className='table--bro' scope='col'>item</th>
@@ -41,13 +83,13 @@ const User = () => {
                             <th className='table--bro' scope='col'>net</th>
                             <th className='table--bro' scope='col'>date</th>
                         </tr>
-                        <tbody className='tbody'>
-                            <tr>
-                                <th></th>
-                            </tr>
-                        </tbody>
                     </thead>
-                </div>
+                    <tbody className='tbody'>
+                        <tr>
+                            <th></th>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     );
